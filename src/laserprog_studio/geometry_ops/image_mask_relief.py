55b0f62a81585b637bb69fc9c5ceb2f9a6a6da67
Matthis-Dubos <119478794+JeanTaniban@users.noreply.github.com>
2026-09-21 13:07:40 +0200

    api: expose mask size and smoothing contracts

--- src/laserprog_studio/geometry_ops/image_mask_relief.py
+++ src/laserprog_studio/geometry_ops/image_mask_relief.py
@@ -3,6 +3,13 @@ from __future__ import annotations
 
 from .image_mask_relief_builder import build_mask_relief_mesh
 from .image_mask_relief_preview import render_mask_preview_image
-from .image_mask_relief_types import MaskReliefResult, MaskReliefStats
+from .image_mask_relief_types import MaskPhysicalSize, MaskReliefResult, MaskReliefStats, MaskSmoothingReport
 
-__all__ = ["MaskReliefResult", "MaskReliefStats", "build_mask_relief_mesh", "render_mask_preview_image"]
+__all__ = [
+    "MaskPhysicalSize",
+    "MaskReliefResult",
+    "MaskReliefStats",
+    "MaskSmoothingReport",
+    "build_mask_relief_mesh",
+    "render_mask_preview_image",
+]

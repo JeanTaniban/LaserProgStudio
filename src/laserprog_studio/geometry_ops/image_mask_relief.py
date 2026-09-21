d2a202ea877e643a186406afae70b62e2341c986
Matthis-Dubos <119478794+JeanTaniban@users.noreply.github.com>
2026-09-21 13:17:54 +0200

    api: expose exact mask preview builder

--- src/laserprog_studio/geometry_ops/image_mask_relief.py
+++ src/laserprog_studio/geometry_ops/image_mask_relief.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 from .image_mask_relief_builder import build_mask_relief_mesh
-from .image_mask_relief_preview import render_mask_preview_image
+from .image_mask_relief_preview import build_mask_preview, render_mask_footprint_image, render_mask_preview_image
 from .image_mask_relief_types import MaskPhysicalSize, MaskReliefResult, MaskReliefStats, MaskSmoothingReport
 
 __all__ = [
@@ -10,6 +10,8 @@ __all__ = [
     "MaskReliefResult",
     "MaskReliefStats",
     "MaskSmoothingReport",
+    "build_mask_preview",
     "build_mask_relief_mesh",
+    "render_mask_footprint_image",
     "render_mask_preview_image",
 ]
